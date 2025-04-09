c2ac90f98b62a16d3a7ae97549ab184336f7f599
superBirdy <cccyyycycyccy@hotmail.com>
2025-04-09 13:25:53 -0500

    init added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=['pandas', 'openpyxl'],
-    author='Your Name',
+    author='Birdy',
     description='Lightweight LCA tool using Excel + IPCC 2021',
     license='MIT',
     include_package_data=True,
